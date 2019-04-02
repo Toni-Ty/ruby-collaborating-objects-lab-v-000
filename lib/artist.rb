e067cf809aec337e0c15b7ec0de372dbf6af395e
Toni-Ty <antonique2479@yahoo.com>
2019-04-02 04:18:15 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -10,15 +10,15 @@ def initialize(name)
 end
 
 def self.find_or_create_by_name(artist_name)
-  artist_found = self.all.find { |artist| artist.name = artist_name}
-  if artist_found
-    artist_found
-  else
-    new_artist = self.new(artist_name)
-    artist_name.save
-    new_artist
-  end
-end
+   artist_found = self.all.find {|artist| artist.name == artist_name}
+   if artist_found
+     artist_found
+   else
+     new_artist = self.new(artist_name)
+     new_artist.save
+     new_artist
+   end
+ end
 
 def add_song(song)
   @songs << song
