ee8fe98953771863c1a1609717ab22104732c74e
Toni-Ty <antonique2479@yahoo.com>
2019-04-02 04:31:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,8 +8,12 @@ end
 
 def self.new_by_filename(filename)
 # new_instance = Song.new_by_filename(file_name)
-name = Song.new(filename)
-
+  song = filename.split(" - ")[1]
+  artist = filename.split(" - ")[0]
+  new_song = self.new(song)
+  new_song_artist = artist
+  new_song
 end
 
+def artists=(name)
 end
