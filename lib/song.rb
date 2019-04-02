859c7d719c91eae977ae94bd7ed617eda055565e
Toni-Ty <antonique2479@yahoo.com>
2019-04-02 04:34:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,19 +6,17 @@ def initialize(name)
   @name = name
 end
 
-def self.new_by_filename(filename)
-# new_instance = Song.new_by_filename(file_name)
-  song = filename.split(" - ")[1]
-  artist = filename.split(" - ")[0]
-  new_song = self.new(song)
-  new_song_artist = artist
-  new_song
-end
+def self.new_by_filename(file_name)
+    song = file_name.split(" - ")[1]
+    artist = file_name.split(" - ")[0]
+    new_song = self.new(song)
+    new_song.artist_name = artist
+    new_song
+  end
 
-def artist_name=(name)
-  self.artist = Artist.find_or_create_by_name(name)
-  artist.add_song(self)
-end
-
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end
 
 end
